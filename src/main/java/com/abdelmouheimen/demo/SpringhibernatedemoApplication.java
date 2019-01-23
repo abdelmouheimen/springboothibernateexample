a6a4a90d01fd8ae8c83f207cf037b7b21eeebe4e
Abdelmouheimen <abdelmouheimen.trabelssi@gmail.com>
2019-01-24 00:48:07 +0100

    initialisation

---
+++ src/main/java/com/abdelmouheimen/demo/SpringhibernatedemoApplication.java
@@ -0,0 +1,14 @@
+package com.abdelmouheimen.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringhibernatedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringhibernatedemoApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/abdelmouheimen/demo/entities/User.java
@@ -0,0 +1,5 @@
+package com.abdelmouheimen.demo.entities;
+
+public class User {
+
+}

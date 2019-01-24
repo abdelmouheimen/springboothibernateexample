7d1646d650c59b3a4199ea09da6e4c87d307f634
Abdelmouheimen <abdelmouheimen.trabelssi@gmail.com>
2019-01-24 01:04:46 +0100

    connection to h2

--- src/main/java/com/abdelmouheimen/demo/entities/User.java
+++ src/main/java/com/abdelmouheimen/demo/entities/User.java
@@ -1,5 +1,29 @@
 package com.abdelmouheimen.demo.entities;
 
-public class User {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+@Entity
+public class User {
+	public User() {
+		
+	}
+	
+	@Id
+	private String id;
+	@Column
+	private String firstName;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 }
